3cc38fc0aaf90786158b817620d13ab854c539e1
hopeson <hopesonogaga@gmail.com>
2025-03-23 18:14:33 +0100

    new nav test

---
+++ src/components/api/news/test.jsx
@@ -0,0 +1,80 @@
+export default function Test (){
+    return <div>
+    <div class="page-content">
+        <section class="page-content_container page-content_container--2-cols">
+            <div class="page-content__leading">
+                <div id="home-container__anchor-top" class="display-none"></div>
+               
+                <article class="page-content_item page-content_item--space">
+                    <div class="article">
+                        <a class="article-teaser article-teaser--small" href="nachricht/aktien/nach-der-wahl-bundestagswahl-2025-boerse-wahlergebnisse-koalitionen-moegliche-auswirkungen-auf-dax-aktien-branchen-maerkte-14256180.html">
+                            <span class="article-teaser__kicker">Nach der Wahl</span>
+                            <span class="article-teaser__media">
+                                <img
+                                    class="article-teaser__image img"
+                                    src="../images.finanzen.net/mediacenter/unsortiert/deutschland-wahl-alexlmx-660-486.jpg"
+                                    width="660"
+                                    height="303"
+                                    title="Nach der Wahl: Bundestagswahl 2025 & Börse: Wahlergebnisse, Koalitionen & mögliche Auswirkungen auf DAX, Aktien, Branchen & Märkte"
+                                    alt="Nach der Wahl: Bundestagswahl 2025 & Börse: Wahlergebnisse, Koalitionen & mögliche Auswirkungen auf DAX, Aktien, Branchen & Märkte"
+                                />
+                            </span>
+                            <h2
+                                class="article-teaser__title font-size-x1.500 font-weight-bold font-size-x1.125 margin-0.00"
+                                title="Bundestagswahl 2025 & Börse: Wahlergebnisse, Koalitionen & mögliche Auswirkungen auf DAX, Aktien, Branchen & Märkte"
+                            >
+                                Bundestagswahl 2025 & Börse: Wahlergebnisse, Koalitionen & mögliche Auswirkungen auf DAX, Aktien, Branchen & Märkte
+                            </h2>
+                            <span class="article-teaser__text">
+                                Die ersten Wahlergebnisse stehen fest, und die Parteien werden nun demnächst Sondierungsgespräche aufnehmen, um mögliche Koalitionen auszuloten. Während die Regierungsbildung noch offen ist, zeichnen sich
+                                bereits verschiedene Bündnisszenarien ab, die maßgeblich die politische und wirtschaftliche Zukunft Deutschlands beeinflussen könnten.
+                            </span>
+                        </a>
+                    </div>
+                </article>
+                
+                <article class="page-content_item page-content_item--space" id="home-container-hif">
+                    <span class="font-color-de-azure-blue font-transform-upper font-size-x0.875">Heute im Fokus</span>
+                    <div class="article">
+                        <a class="article-teaser article-teaser--small" href="nachricht/aktien/heute-im-fokus-24-02-2025-14261867.html">
+                            <h2
+                                class="article-teaser__title font-size-x1.250 margin-bottom-0.00 font-weight-bold font-size-x1.125 margin-0.00"
+                                title="Hoffnung auf Politikwechsel: DAX schließt fester -- Union bei Bundestagswahl klar vorn -- Warren Buffetts Berkshire Hathaway mit Gewinnrückgang -- Just Eat wird von Prosus übernommen"
+                            >
+                                Hoffnung auf Politikwechsel: DAX schließt fester -- Union bei Bundestagswahl klar vorn -- Warren Buffetts Berkshire Hathaway mit Gewinnrückgang -- Just Eat wird von Prosus übernommen
+                            </h2>
+                            <span class="article-teaser__text">
+                                <span class="display-none-md">
+                                    Goldpreis steigt auf neues Allzeithoch. DHL expandiert im Nahen Osten. KI-Investitionspläne und mögliche EU-Kartellklage bewegen US-Tech-Aktien. Microsoft fährt offenbar Investitionen in KI-Datenzentren
+                                    zurück - Siemens Energy-Aktie leidet. Pfandbriefbank verpflichtet neuen Risikochef von der Deutschen Bank. Alibaba plant Milliardeninvestment in Cloud und KI. Baywa-Rettung wird länger dauern als geplant.
+                                </span>
+                            </span>
+                        </a>
+                    </div>
+                    <span class="font-size-x0.750 font-color-gray display-none display-block-md">zuletzt aktualisiert vor 4 Minuten</span>
+                </article>
+                <div class="page-layout__ad-content sticky display-none display-block-md margin-horizontal-1.00" data-appnexus="m_mrec" data-type="dynamic-content">
+                    <span class="ad-hint margin-start-1.00 display-block">Wer&shy;bung</span>
+                    <div class="ad sticky_container sticky_container--mrec">
+                        <div class="sticky__item"><div id="m_mrec" data-appnexus-container=""></div></div>
+                    </div>
+                </div>
+                <div id="chartbox-placeholder" class="display-none display-block-md"></div>
+                <div class="page-content_item--space page-layout_ad-content sticky display-none display-block-md margin-horizontal-1.00" data-appnexus="m_mrec_btf" data-type="dynamic-content">
+                    <span class="ad-hint margin-start-1.00 display-block">Wer&shy;bung</span>
+                    <div class="ad sticky_container sticky_container--mrec-btf">
+                        <div class="sticky__item"><div id="m_mrec_btf" data-appnexus-container=""></div></div>
+                    </div>
+                </div>
+                <div id="real-time-indication-placeholder" class="display-none display-block-md"></div>
+                <div class="page-content_item--space page-layout_ad-content sticky display-none display-block-md margin-horizontal-1.00" data-appnexus="m_mrec_btf" data-type="dynamic-content">
+                    <span class="ad-hint margin-start-1.00 display-block">Wer&shy;bung</span>
+                    <div class="ad sticky_container sticky_container--mrec-btf">
+                        <div class="sticky__item"><div id="m_mrec_btf_2" data-appnexus-container=""></div></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
+} 
